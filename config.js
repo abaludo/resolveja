f69da5855a9705eec7270ac826f5286593c1f40e
abaludo <emanuelvasconcelos098@gmail.com>
2026-09-14 23:34:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,5 @@
 window.RESOLVEJA_CONFIG = {
   SUPABASE_URL: "https://tgfbqupkyjmfoougddmc.supabase.co",
-  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_OXXZ4r4_FNlojpss_8f8kQ_jAnkpUek"
+  SUPABASE_PUBLISHABLE_KEY: "sb_publishable_OXXZ4r4_FNlojpss_8f8kQ_jAnkpUek",
+  TURNSTILE_SITE_KEY: "0x4AAAAAAE0nlz-sOxaKCCmg"
 };
